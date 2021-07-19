c9f50402abb448bd7a5a86a2359f8b31b350a735
HB <85717438+HBAMAL@users.noreply.github.com>
2021-07-19 11:20:58 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -213,7 +213,7 @@ async def auto_filter(bot, update):
             await bot.send_photo(
                 chat_id = update.chat.id,
                 photo="https://telegra.ph/file/1d5776dc7737830c675fe.jpg",
-                caption= f"📁FILE NAME📁:</i> <code>{query}</code>  
+                caption= f"📁FILE NAME📁:<code>{query}</code>"
                 🗂️ TOTAL FILES FOUND :- {(len_results)} 
                 👉 <a href='https://t.me/joinchat/UZzc1UhZLUnvorhW'> ⚠️ERROR JOIN THIS CHANNEL FIRST⚠️</a>",
                 reply_markup=reply_markup,

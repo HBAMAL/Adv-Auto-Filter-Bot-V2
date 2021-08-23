95d6f4c220b84db6c3355d867427e2abaf4559f6
HB <85717438+HBAMAL@users.noreply.github.com>
2021-08-23 13:03:40 +0530

    Update broadcast.py

--- bot/plugins/broadcast.py
+++ bot/plugins/broadcast.py
@@ -5,12 +5,29 @@ import logging
 import aiofiles
 from pyrogram import Client, filters
 
-from bot.bot import Bot
+BOT_OWNER = Bot.BOT_OWNER
 
-from bot.database import Database # pylint: disable=import-error
+
+from bot.database import Database
 from bot.bot import Bot 
+
 broadcast_ids = {}
 
+async def send_msg(user_id, message):
+	try:
+		await message.forward(chat_id=user_id)
+		return 200, None
+	except FloodWait as e:
+		await asyncio.sleep(e.x)
+		return send_msg(user_id, message)
+	except InputUserDeactivated:
+		return 400, f"{user_id} : deactivated\n"
+	except UserIsBlocked:
+		return 400, f"{user_id} : blocked the bot\n"
+	except PeerIdInvalid:
+		return 400, f"{user_id} : user id invalid\n"
+	except Exception as e:
+		return 500, f"{user_id} : {traceback.format_exc()}\n"
 
 
 @Client.on_message(filters.private & filters.command("broadcast") & filters.reply)

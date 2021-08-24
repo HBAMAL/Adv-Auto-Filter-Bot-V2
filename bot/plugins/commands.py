55fe75e73a055f8000630de902cb9546301318d7
HB <85717438+HBAMAL@users.noreply.github.com>
2021-08-24 23:09:40 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -170,7 +170,7 @@ async def help(bot, update):
 async def source(bot, update):
     await bot.send_sticker(
         chat_id=update.chat.id,
-        sticker='CAACAgUAAxkBAAPBYSUrrGzEEKDhsQT8n0HEFZpeygsAAp0FAALF8XlUa1LObSUfMnseBA',
+        sticker='CAADBQADMwIAAtbcmFelnLaGAZhgBwI',
         reply_to_message_id=update.message_id
     )
 

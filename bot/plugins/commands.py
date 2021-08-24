595b47a9525e19943869effcc9cd0f2c2f03f64f
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-08-24 18:11:54 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -9,7 +9,7 @@ from pyrogram.errors import UserNotParticipant, ChatAdminRequired, UsernameNotOc
 
 
 from bot.database import Database # pylint: disable=import-error
-from config import Config
+from config import UPDATE_CHANNEL
 
 db = Database()
 

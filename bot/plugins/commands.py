8b156842be47a9bcd7c5c167943f5cf0609b90d9
HB <85717438+HBAMAL@users.noreply.github.com>
2021-08-24 09:49:57 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -133,7 +133,7 @@ async def help(bot, update):
         text=Translation.HELP_TEXT,
         reply_markup=reply_markup,
         parse_mode="html",
-        disable_web_page_preview=true,
+        disable_web_page_preview=True,
         reply_to_message_id=update.message_id
     )
 

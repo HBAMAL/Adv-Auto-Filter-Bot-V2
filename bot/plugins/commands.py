bb125fd84ee4874f8aef0d2da1df9ce2afddd75b
HB <85717438+HBAMAL@users.noreply.github.com>
2021-08-27 00:49:29 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -155,7 +155,7 @@ async def start(bot, update):
     )
 
 
-@Client.on_message(filters.command(["ping"]) & filters.private, group=2)
+@Client.on_message(filters.command(["ping"]) & filters.private, group=1)
 async def ping(bot, update):
     start = datetime.now()
     tauk = await update.reply_text('CHECKING...')
